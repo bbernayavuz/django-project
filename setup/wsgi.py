ea65d6f8b05dddfc45c4f5084346d1846e0fa926
bbernayavuz <bberna.yavuz@gmail.com>
2022-05-13 23:50:34 +0300

    first app

--- setup/wsgi.py
+++ setup/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for brn_blog project.
+WSGI config for setup project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'brn_blog.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'setup.settings')
 
 application = get_wsgi_application()
